0fa72ec654fc556f87accf4b906e15562026f47d
Your Name <you@example.com>
2018-08-14 12:37:28 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.destroy_all
+Coupon.create(coupon_code: "123", store: "Walmart")
+Coupon.create(coupon_code: "456", store: "Amazon")
+Coupon.create(coupon_code: "789", store: "Essen")
+Coupon.create(coupon_code: "789", store: "Ebaby")
+Coupon.create(coupon_code: "789", store: "Whole Food")
+Coupon.create(coupon_code: "78qd9", store: "Ikea")
+Coupon.create(coupon_code: "78ada9", store: "Chi")
